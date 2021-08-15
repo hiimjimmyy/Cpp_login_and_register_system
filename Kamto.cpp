099221cfcc16fd1a03e85390d6bd7f33451af9ee
jimmy <74153672+hiimjimmyy@users.noreply.github.com>
2021-08-15 12:59:26 +0200

    update

--- Kamto.cpp
+++ Kamto.cpp
@@ -31,9 +31,13 @@ bool isLoggedIn(fstream & x)
         x.close();
 
         if (un == username && pw == password)
+        {
             return true;
+        }
         else
+        {
             return false;
+        }
     }
 }
 
@@ -42,13 +46,13 @@ int main()
     int choice;
 
     //provide login or registration option for returning or new users
-    cout << "Please select one of the following options: ";
+    cout << "\nPlease select one of the following options: ";
     cout << "\n1. Register\n2. Login\n"; 
     cin >> choice;
     
-    while (choice < 1 or choice > 2)
+    while (choice < 1 || choice > 2)
     {
-        cout << "Please enter a valid input, options 1 or 2.";
+        cout << "Please enter a valid input, options 1 or 2.\n";
         cin >> choice;
     }
             
@@ -56,8 +60,8 @@ int main()
     {
         string username, password;
 
-        cout << "Please enter a desired username: "; cin >> username;
-        cout << "Please enter a desired password: "; cin >> password;
+        cout << "\nPlease enter a desired username: "; cin >> username;
+        cout << "\nPlease enter a desired password: "; cin >> password;
 
         //this will create a file to add new users
         //ofstream to write new data into text file
@@ -79,13 +83,13 @@ int main()
         
         if(!status)
         {
-            cout << "The login infomation is incorrect, please try again." << endl;
+            cout << "\nThe login infomation is incorrect, please try again.\n" << endl;
             system("PAUSE");            
             return 0;
         }
         else
         {
-            cout << "Login successful!" << endl;
+            cout << "\nLogin successful!" << endl;
             system("PAUSE");            
             return 1;
         }
